432fe06dc0d3133bf6b68afe6d67b2e7c6c36ab8
mrwilsondj <mrwilsondj@gmail.com>
2014-07-14 12:19:08 -0400

    fixed page header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = "#{current_user.oauth_token}"
-      config.access_token_secret = "#{current_user.oauth_secret}"
+      # config.access_token        = "#{current_user.oauth_token}"
+      # config.access_token_secret = "#{current_user.oauth_secret}"
+      config.access_token        = ENV["TWITTER_OAUTH_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_OAUTH_SECRET"]
     end
 
     @friends = client.friends.take(20)
