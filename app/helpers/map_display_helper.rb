392af23497d1692ccae7fe015c90ac122452ffd7
mrwilsondj <mrwilsondj@gmail.com>
2014-07-14 20:10:46 -0400

    fixed map markers

--- app/helpers/map_display_helper.rb
+++ app/helpers/map_display_helper.rb
@@ -1,5 +1,5 @@
 module MapDisplayHelper
   def get_marker_data(screen_name,name)
-    "<strong>Twitter handle:</strong>#{screen_name}<br/><strong>Name:</strong> Name: #{name}"
+    "<strong>Name:</strong> #{name} <br/><strong>Twitter handle:</strong>#{screen_name}"
   end
 end
