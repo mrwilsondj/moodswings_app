fe601a2028a192800a054f476f14a85f0b9830f1
slaffont <slaffont@gmail.com>
2014-07-16 14:48:31 -0400

    take out location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,16 @@ class HomeController < ApplicationController
   def index
     if not params[:username].nil?
       client = Twitter::REST::Client.new do |config|
+          config.consumer_key   = ENV['CONSUMER_KEY']
+    config.consumer_key = ENV['CONSUMER_SECRET']
         config.consumer_key        = 'Rd5s5s82FAiUD1KufnrnQ'
         config.consumer_secret     = '6q8LouMcq8qE1aZa5Mn5nONdwpzchrmXOIlqEYl9CU'
         config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
         config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+        #   config.consumer_key        = 'Rd5s5s82FAiUD1KufnrnQ'
+        # config.consumer_secret     = '6q8LouMcq8qE1aZa5Mn5nONdwpzchrmXOIlqEYl9CU'
+        # config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        # config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
       end
 
       # try to find a twitter user
@@ -51,8 +57,10 @@ class HomeController < ApplicationController
 
   def fetch_friend_data
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = 'Rd5s5s82FAiUD1KufnrnQ'
-      config.consumer_secret     = '6q8LouMcq8qE1aZa5Mn5nONdwpzchrmXOIlqEYl9CU'
+     config.consumer_key   = ENV['CONSUMER_KEY']
+    config.consumer_key = ENV['CONSUMER_SECRET']
+      # config.consumer_key        = 'Rd5s5s82FAiUD1KufnrnQ'
+      # config.consumer_secret     = '6q8LouMcq8qE1aZa5Mn5nONdwpzchrmXOIlqEYl9CU'
     #   # config.access_token        = "#{current_user.oauth_token}"
     #   # config.access_token_secret = "#{current_user.oauth_secret}"
       config.access_token        = ENV["TWITTER_OAUTH_TOKEN"]
