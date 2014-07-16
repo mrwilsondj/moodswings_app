f512153138a5a6a094193f7ca18f40ad6cc87837
slaffont <slaffont@gmail.com>
2014-07-16 14:41:16 -0400

    commented out api fetch tweet2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -50,14 +50,14 @@ class HomeController < ApplicationController
   end
 
   def fetch_friend_data
-    # client = Twitter::REST::Client.new do |config|
+    client = Twitter::REST::Client.new do |config|
     #   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
     #   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
     #   # config.access_token        = "#{current_user.oauth_token}"
     #   # config.access_token_secret = "#{current_user.oauth_secret}"
-    #   config.access_token        = ENV["TWITTER_OAUTH_TOKEN"]
-    #   config.access_token_secret = ENV["TWITTER_OAUTH_SECRET"]
-    # end
+      config.access_token        = ENV["TWITTER_OAUTH_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_OAUTH_SECRET"]
+    end
 
     @friends = client.friends.take(20)
 
