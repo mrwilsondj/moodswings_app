fb440b0c054d3eaa4cc9ca544382bd618487f5f2
slaffont <slaffont@gmail.com>
2014-07-30 14:34:43 -0400

    july30

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
-
-
 class HomeController < ApplicationController
   def index
     if not params[:username].nil?
-     client = Twitter::REST::Client.new do |config|
+      client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
         config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
         config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
@@ -52,7 +50,7 @@ class HomeController < ApplicationController
   end
 
   def fetch_friend_data
-     client = Twitter::REST::Client.new do |config|
+    client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
       # config.access_token        = "#{current_user.oauth_token}"
